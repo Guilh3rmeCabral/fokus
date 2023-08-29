1a75d7750da75f907a63fe6c686ee20b4f63c550
Guilherme Cabral <guicabralprowork@gmail.com>
2023-08-29 19:17:12 -0300

    feat: adicionando a classe para ativar a classe do botão, e adicionando evento de musica.

--- js/main.js
+++ js/main.js
@@ -2,22 +2,46 @@ const html = document.querySelector('html');
 const focoBt = document.querySelector('.app__card-button--foco');
 const curtoBt = document.querySelector('.app__card-button--curto');
 const longoBt = document.querySelector('.app__card-button--longo');
-const banner = document.querySelector('.app__image')
-const titulo = document.querySelector('.app__title')
+const banner = document.querySelector('.app__image');
+const titulo = document.querySelector('.app__title');
+const btnAtivado = document.querySelectorAll('.app__card-button');
+const musicaFoco = document.querySelector('#alternar-musica');
+const musica = new Audio('/sons/luna-rise-part-one.mp3');
+musica.loop = true;
+
+// const startPause = document.querySelector('#start-pause');
+
+// let tempoId = null;
+// let tempoTotal = 5
+
+
+musicaFoco.addEventListener('change', () => {
+    if(musica.paused) {
+        musica.play()
+    } else {
+        musica.pause()
+    }
+})
 
 focoBt.addEventListener('click', () => {
-    mudandoContexto('foco');   
+    mudandoContexto('foco');  
+    focoBt.classList.add('active'); 
 })
 
 curtoBt.addEventListener('click', () => {
     mudandoContexto('descanso-curto');
+    curtoBt.classList.add('active')
 })
 
 longoBt.addEventListener('click', () => {
-    mudandoContexto('descanso-longo')
+    mudandoContexto('descanso-longo');
+    longoBt.classList.add('active')
 })
 
 function mudandoContexto(contexto) {
+    btnAtivado.forEach((contexto) => {
+        contexto.classList.remove('active');
+    })
     html.setAttribute('data-contexto', contexto);
     banner.setAttribute('src', `/imagens/${contexto}.png`);
     switch (contexto) {
@@ -33,3 +57,15 @@ function mudandoContexto(contexto) {
             break;
     }
 }
+
+// const contagemRegressiva = () => {
+//     iniciar()
+//     tempoTotal -= 1;
+// }
+
+// startPause.addEventListener('click', contagemRegressiva);
+
+// function iniciar () {
+//     tempoId = setInterval (contagemRegressiva, 1000)
+// }
+
